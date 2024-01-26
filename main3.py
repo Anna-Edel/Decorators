e14519338d264778132f2326c5524e28bd1d5d4a
Anna-Edel <125572175+Anna-Edel@users.noreply.github.com>
2024-01-26 13:13:25 +0100

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,25 @@
+# Применить написанный логгер к приложению из любого предыдущего д/з.
+
+from main2 import logger
+
+path = 'logger.log'
+
+
+@logger(path)
+def flat_generator(list_of_lists):
+    cursor = 0
+    while cursor < len(list_of_lists):
+        list_ = list_of_lists[cursor]
+        for element in list_:
+            yield element
+        cursor += 1
+
+
+if __name__ == '__main__':
+    list_of_lists = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    print(list(flat_generator(list_of_lists)))
